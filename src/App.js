7d94d1f1f6b0a205ca86815a337566d4b7f4ccd6
alexkkm <alexkong0222@gmail.com>
2024-10-21 01:58:01 +0800

    trying to simple do code understand the css rules

--- src/App.js
+++ src/App.js
@@ -1,45 +1,23 @@
+import React from 'react';
+import Button from './Button.jsx';
 import './App.css';
 
-// Basic tools 
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-
-// Page of the app
-import SemanticUIPage from './SemanticUIPage';
-import FirebasePage from './components/FirebasePage';
-import Test from './Test';
-import FontTemplate from './fonts/font';
-import CyberpunkPage from './cyberpunk_theme/cyberpunk';
-import DarkThemePage from './cyberpunk_theme/darktheme';
-
-const HomePage = () => {
+const App = () => {
   return (
-    <>
-      <Link to="/semantic-ui">Semantic-UI</Link>
-      <hr />
-      <Link to="/firebase">Firebase</Link>
-      <hr />
-      <Link to="/cyberpunk">Cyberpunk Theme</Link>
-      <hr />
-      <Link to="/darktheme">Dark Cyberpunk theme</Link>
-      <hr />
-      <Link to="/test">Test</Link>
-    </>)
-}
+    <div className='App'>
+      <h1>Hello React.</h1>
+      <h2>Start editing to see some magic happen!</h2>
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />}>Home Page</Route>
-        <Route path="/home" element={<HomePage />}>Home Page</Route>
-        <Route path="/test" element={<Test />} />
-        <Route path="/semantic-ui" element={<SemanticUIPage />} />
-        <Route path="/firebase" element={<FirebasePage />} />
-        <Route path="/font" element={<FontTemplate />} />
-        <Route path="/cyberpunk" element={<CyberpunkPage />} />
-        <Route path="/darktheme" element={<DarkThemePage />} />
-      </Routes>
-    </BrowserRouter>
+      <div className='playground'>
+        <Button
+          id='testing'
+          label='Hi'
+          onClick={() => {
+            console.log('Hellow');
+          }}
+        />
+      </div>
+    </div>
   );
 }
 
--- src/Test.js
+++
@@ -1,9 +0,0 @@
-import "./test.css"
-
-const Test = () => {
-    return (
-        <p>Test Page</p>
-    )
-}
-
-export default Test;
